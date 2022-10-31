4e947f8fdd4b2800a7a7f6263a02beeab261d32e
notpresident35 <notpresident35@gmail.com>
2022-10-30 21:48:34 -0700

    Add FireResult API to Gun

---
+++ Assets/Scripts/Magnetics/FireResult.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct FireResult
+{
+	public List<Anchor> AvailableTargets;
+	public Anchor SelectedTarget;
+	public Vector3 CursorOrigin;
+}
