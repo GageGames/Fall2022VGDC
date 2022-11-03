cb092cf019876d32512f1fecdc9be655174c30b3
Michelle <michelleepak@gmail.com>
2022-11-02 17:07:13 -0700

    game over screen
    game over screen

---
+++ Assets/Developers/michelle/dotweenpractice.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class dotweenpractice : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //transform.DOMove(new Vector3(10,2,2), .5f).SetLoops(-1,LoopType.Yoyo).SetEase(Ease.InBounce);
+       // transform.DOScale(new Vector3(5, 5, 5), 5).SetEase(Ease.OutSine);
+        //transform.DOScale(Vector3.zero, 2);
+    }
+
+    // Update is called once per frame
+   // void Update()
+    //{
+        
+    //}
+}
