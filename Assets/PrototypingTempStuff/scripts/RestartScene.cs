75c306c81e92872b15dba6978dd16e0239099ddb
Ryan <ryan_w2012@yahoo.com>
2022-11-05 15:04:41 -0700

    Tutorial testing
    Tutorial Level will be completely reworked.

---
+++ Assets/PrototypingTempStuff/scripts/RestartScene.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartScene : MonoBehaviour
+{
+
+	public string scene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+			SceneManager.LoadScene(scene);
+		}
+    }
+
+
+}
