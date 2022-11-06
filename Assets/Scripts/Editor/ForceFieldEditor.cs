10d3f09794b8116b76b96b1f6a73873f82a9a7c5
notpresident35 <notpresident35@gmail.com>
2022-11-05 21:15:52 -0700

    Add explosive entity, apply formatting

--- Assets/Scripts/Editor/ForceFieldEditor.cs
+++ Assets/Scripts/Editor/ForceFieldEditor.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor;
 using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
 
 [CanEditMultipleObjects]
 [CustomEditor(typeof(Forcefield))]
--- Assets/Scripts/Gameplay/Spawnpoint.cs
+++ Assets/Scripts/Gameplay/Spawnpoint.cs
@@ -7,50 +7,50 @@ using UnityEditor;
 [ExecuteAlways]
 public class Spawnpoint : MonoBehaviour
 {
-    public float SpawnRadius = 3f;
+	public float SpawnRadius = 3f;
 
-    public SpawnpointType spawnpointType;
-    public SpawnpointEditorPreviewData editorPreviewData;
+	public SpawnpointType spawnpointType;
+	public SpawnpointEditorPreviewData editorPreviewData;
 
-    public Color customDiscColor;
-    public float customDiscThickness;
+	public Color customDiscColor;
+	public float customDiscThickness;
 
-    private void OnEnable () => SpawnpointManager.ActiveSpawnpoints.Add (this);
-    private void OnDisable () => SpawnpointManager.ActiveSpawnpoints.Remove (this);
+	private void OnEnable() => SpawnpointManager.ActiveSpawnpoints.Add(this);
+	private void OnDisable() => SpawnpointManager.ActiveSpawnpoints.Remove(this);
 
-    private void OnValidate ()
-    {
-        ApplyCustomEditorPreviewData ();
-    }
+	private void OnValidate()
+	{
+		ApplyCustomEditorPreviewData();
+	}
 
-    public void ApplySpawnpointType ()
-    {
-        if (spawnpointType != SpawnpointType.Custom)
-        {
-            editorPreviewData = Resources.Load ($"Data/Spawnpoints/{spawnpointType.ToString ()}") as SpawnpointEditorPreviewData;
-        }
-        else
-        {
-            editorPreviewData = Instantiate(Resources.Load ($"Data/Spawnpoints/Neutral") as SpawnpointEditorPreviewData);
-        }
+	public void ApplySpawnpointType()
+	{
+		if (spawnpointType != SpawnpointType.Custom)
+		{
+			editorPreviewData = Resources.Load($"Data/Spawnpoints/{spawnpointType.ToString()}") as SpawnpointEditorPreviewData;
+		}
+		else
+		{
+			editorPreviewData = Instantiate(Resources.Load($"Data/Spawnpoints/Neutral") as SpawnpointEditorPreviewData);
+		}
 
-        customDiscColor = editorPreviewData.discColor;
-        customDiscThickness = editorPreviewData.discThickness;
-    }
+		customDiscColor = editorPreviewData.discColor;
+		customDiscThickness = editorPreviewData.discThickness;
+	}
 
-    public void ApplyCustomEditorPreviewData ()
-    {
-        editorPreviewData.discColor = customDiscColor;
-        editorPreviewData.discThickness = customDiscThickness;
-    }
-    
+	public void ApplyCustomEditorPreviewData()
+	{
+		editorPreviewData.discColor = customDiscColor;
+		editorPreviewData.discThickness = customDiscThickness;
+	}
+
 #if UNITY_EDITOR
-    private void OnDrawGizmosSelected ()
-    {
-        Color colorCache = Handles.color;
-        Handles.color = editorPreviewData.discColor;
-        Handles.DrawWireDisc (transform.position, transform.up, SpawnRadius, editorPreviewData.discThickness);
-        Handles.color = colorCache;
-    }
+	private void OnDrawGizmosSelected()
+	{
+		Color colorCache = Handles.color;
+		Handles.color = editorPreviewData.discColor;
+		Handles.DrawWireDisc(transform.position, transform.up, SpawnRadius, editorPreviewData.discThickness);
+		Handles.color = colorCache;
+	}
 #endif
 }
