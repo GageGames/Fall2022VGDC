ba10290cb2db4b6d342c09038fc2a9cea6841554
notpresident35 <notpresident35@gmail.com>
2022-11-05 20:20:57 -0700

    Add force field editor tool

--- Assets/Scripts/Physics/Forcefield.cs
+++ Assets/Scripts/Physics/Forcefield.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 //Pushes movable objects in a direction. (Functions like a fan)
 public class Forcefield : MonoBehaviour
 {
-    [SerializeField] float strength;
-    [SerializeField] Vector3 direction;
+    public float strength;
+    public Vector3 direction;
     static ImpulseSourceType type = new ImpulseSourceType(ImpulseSourceTag.Field);
 
-    private void OnTriggerEnter(Collider other) {
+	private void OnTriggerEnter(Collider other) {
         Transform Othert = other.transform;
         while (Othert.parent != null)
         {
@@ -20,4 +21,14 @@ public class Forcefield : MonoBehaviour
             receiver.ApplyImpulse(direction, strength, type);
         }    
     }
+
+#if UNITY_EDITOR
+	private void OnDrawGizmosSelected()
+	{
+		Color colorCache = Handles.color;
+		Handles.color = Color.Lerp(Color.green, Color.red, Mathf.Clamp01(strength / 8f));
+		Handles.DrawLine(transform.position, transform.position + direction * strength, 3 + 5f * Mathf.Clamp01(strength / 8f));
+		Handles.color = colorCache;
+	}
+#endif
 }
