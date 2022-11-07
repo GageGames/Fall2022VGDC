0bc3bac783e580898e9daf032a0f4544a2a83e69
Ryan <ryan_w2012@yahoo.com>
2022-11-06 20:47:29 -0800

    Tutorial Prototype

--- Assets/PrototypingTempStuff/scripts/RestartScene.cs
+++ Assets/PrototypingTempStuff/scripts/RestartScene.cs
@@ -16,7 +16,7 @@ public class RestartScene : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		if (Input.GetKeyDown(KeyCode.Escape))
+		if (Input.GetKeyDown(KeyCode.F))
 		{
 			SceneManager.LoadScene(scene);
 		}
